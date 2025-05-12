951ce2512fe88f82507d36e9af97101ff5de9cc2
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2025-05-12 12:42:11 -0300

    refactor: self command

--- BLM.lua
+++ BLM.lua
@@ -84,8 +84,6 @@ function get_sets()
     ---------------
 	-- Variables --
 	---------------
-
-	display_mp_cost = true
     
    	use_terras = false
 
@@ -359,31 +357,17 @@ function self_command(command)
 		meleeMode:cycle() -- go to next
 		
 		local temp = meleeMode.value
-		if temp == 'club' or temp == 'staffacc' or temp == 'staffhaste'then
+		if temp == 'melee' then
 			enable('main','sub','range','ammo')
-			if temp == 'club' then
-				equip(sets.weapons.club)
-			elseif temp == 'staffacc' or temp == 'staffhaste' then
-				equip(sets.weapons.staff)
-			end
-			disable('main','sub','range','ammo')
-			windower.add_to_chat(122,'Meleeing in ' .. meleeMode.current)
+            equip(sets.weapons.staff)
+            disable('main','sub','range','ammo')
+			windower.add_to_chat(122,'Melee Mode on')
 			
-		elseif temp == 'mage' then -- mage mode
+		elseif temp == 'nuke' then -- mage mode
 			enable('main','sub','range','ammo')
-			windower.add_to_chat(122,'Mage mode')
+			windower.add_to_chat(122,'Mage mode on')
 		end
 		choose_set()
-
-	-- Potency/skill nukes
-    elseif command == "nukeDmg" then
-		if nukeDmg == false then
-			nukeDmg = true
-			windower.add_to_chat(122,'Nuking in potency')
-		else
-			nukeDmg = false
-			windower.add_to_chat(122,'Nuking in skill')
-		end
 		
 	-- Induce yellow HP
 	elseif command == "toyellowHP" then
@@ -392,16 +376,6 @@ function self_command(command)
 		else
 			equip_yellow()
 		end
-	
-	-- Toggle MP info
-	elseif command == "dispmp" then
-		if display_mp_cost then
-			display_mp_cost = false
-			windower.add_to_chat(122,'Stop showing magic MP cost.')
-		else
-			display_mp_cost = true
-			windower.add_to_chat(122,'Show magic MP cost.')
-		end	
 	
 	-- Toggle Terra's/Claustrum in idle
 	elseif command == "use_terras" then
@@ -413,12 +387,6 @@ function self_command(command)
 			windower.add_to_chat(122,'Using Terra\'s Staff in idle')
 		end	
 		choose_set()
-			
-	-- Clipping plane (requires Config plugin)
-	elseif command == "clippingPlane" then
-		clippingPlane:cycle()
-		send_command('input //config ClippingPlane ' .. clippingPlane.value)
-		windower.add_to_chat(122,"ClippingPlane: " .. clippingPlane.current)
 	end
 		
 end
@@ -428,4 +396,4 @@ end
 ---------------
 
 enable('main','sub','range','ammo','head','neck','left_ear','right_ear','body','hands','left_ring','right_ring','back','waist','legs','feet')
-send_command('wait 1; input /cm u;wait 1;gs equip idle;wait 1;input /macro book 2;wait 2; input !myth; wait 1; input /echo Gearswap loaded.')
+send_command('wait 1; input /cm u;wait 1;gs equip idle;wait 2;gs equip weapon.staff;wait 1; input !myth; wait 1; input /echo Gearswap loaded.')
